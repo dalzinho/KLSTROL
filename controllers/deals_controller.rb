b40b15e86f56e2908e010394621a5eb1fe8320a8
John Daly <jpdaly@gmail.com>
2017-02-05 20:23:47 +0000

    burgers functionality is viable-ish! added index, new and select to deals

--- controllers/deals_controller.rb
+++ controllers/deals_controller.rb
@@ -6,3 +6,18 @@ get '/deals' do
   @deals = Deal.all
   erb (:"deals/index")
 end
+
+get '/deals/new' do
+  erb(:"deals/new")
+end
+
+post '/deals' do
+  @deal = Deal.new(params)
+  @deal.save
+  redirect to ('/deals')
+end
+
+get '/deals/:id' do
+  @deal = Deal.find(params[:id])
+  erb(:"deals/show")
+end
--- models/Deal.rb
+++ models/Deal.rb
@@ -1,5 +1,6 @@
 class Deal
 
+  attr_reader :id
   attr_accessor :title, :burger_id, :day
 
   def initialize(options)
@@ -30,5 +31,21 @@ class Deal
     sql = "DELETE FROM deals WHERE id = #{@id}"
     SqlRunner.run(sql)
   end
+
+  def get_burger
+    sql = "SELECT b.name FROM burgers b INNER JOIN deals d ON d.burger_id = b.id WHERE d.id = #{@id};"
+    return SqlRunner.run(sql).first['name']
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM deals WHERE id = #{id};"
+    result = SqlRunner.run(sql).first
+    return Deal.new(result)
+  end
+
+  # def get_vendor
+  #   sql = "SELECT v.name FROM vendors v INNER JOIN deals d ON d.vendor_id = v.id WHERE d.id = #{@id};"
+  #   return SqlRunner.run(sql).first['name']
+  # end
 
 end
